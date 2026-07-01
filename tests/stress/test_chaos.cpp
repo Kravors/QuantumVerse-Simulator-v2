da1fef405f362953cbaf5a2639bd17f31411a73d
kravors <dhiae1122@gmail.com>
2026-07-01 17:11:13 +0100

    fix: Add M_PI definition for MSVC

--- tests/stress/test_chaos.cpp
+++ tests/stress/test_chaos.cpp
@@ -6,6 +6,10 @@
 #include "spacetime/MetricTensor.h"
 #include "spacetime/Event4D.h"
 
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+
 using namespace quantumverse;
 
 std::atomic<bool> g_running(true);
